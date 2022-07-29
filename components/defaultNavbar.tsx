2f46edbd66e3b2e296c7c5902dd54cec8bcee73f
Jeremy Warren <jeremyawarren15@gmail.com>
2022-07-29 01:12:43 -0400

    fixed build failure

--- components/defaultNavbar.tsx
+++ components/defaultNavbar.tsx
@@ -1,4 +1,5 @@
 import { FunctionComponent } from 'react';
+import Image from 'next/image';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -9,7 +10,7 @@ const DefaultNavbar: FunctionComponent = () => {
     <Navbar collapseOnSelect expand="lg" variant="dark" style={{backgroundColor: "#d10a1b"}}>
       <Container>
         <Navbar.Brand href="/" className={style.brand}>
-          <img
+          <Image
             alt="TSG Shield Logo"
             src="TSG_shield.png"
             width="40px"
@@ -20,7 +21,7 @@ const DefaultNavbar: FunctionComponent = () => {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link href="https://troopsofsaintgeorge.org/wp-content/uploads/2020/09/2020-09-28-TSG-Officers-Manual-2020-Ed.pdf">Officer's Manual</Nav.Link>
+            <Nav.Link href="https://troopsofsaintgeorge.org/wp-content/uploads/2020/09/2020-09-28-TSG-Officers-Manual-2020-Ed.pdf">Officer&#39s Manual</Nav.Link>
           </Nav>
           <Nav>
             <Nav.Link href="https://docs.google.com/forms/d/e/1FAIpQLScvqY4kwtT_bvMHGplOFZ7YYS8KayHzdocagW_HN0TrpraruQ/viewform?usp=sf_link">Register</Nav.Link>
--- pages/index.tsx
+++ pages/index.tsx
@@ -33,7 +33,7 @@ const Home: NextPage = () => {
                 <Col sm={2} />
                 <Col sm={5}>
                   <h1>TSG - Troop 68</h1>
-                  <p>"He has shown you, O mortal, what is good. And what does the LORD require of you? To act justly and to love mercy and to walk humbly with your God."</p>
+                  <p>&quotHe has shown you, O mortal, what is good. And what does the LORD require of you? To act justly and to love mercy and to walk humbly with your God.&quot</p>
                   <p>- Micah 6:8</p>
                 </Col>
               </Row>
@@ -44,11 +44,11 @@ const Home: NextPage = () => {
           <Row>
             <h2>What is Troops of Saint George?</h2>
             <p>Pulling from the Troops of Saint George website</p>
-            <p>"The Troops of Saint George is a fraternal Catholic nonprofit apostolate for priests, men, and young men looking for a life of adventure coupled with virtue."</p>
+            <p>&quotThe Troops of Saint George is a fraternal Catholic nonprofit apostolate for priests, men, and young men looking for a life of adventure coupled with virtue.&quot</p>
             <p>Troop 68 is the first troop in the Indianapolis area for Troops of Saint George. Our home parish is St. John the Evangelist Catholic Church in downtown Indianapolis, but we are open to members from other parishes at this time.</p>
 
             <h2>Troops of St. George Mission Statement</h2>
-            <p>"The Troops of Saint George apostolate aims to use the outdoors as our canvas and the sacraments as our path to light the way for the formation of Holy Catholic men and boys. Whether called to the vocation of the priesthood, the religious life, or that of Holy fatherhood, our fathers and sons will take a prayerful pilgrimage together to fulfill Christ's desire for them to grow in virtue and in their Holy Catholic faith as they journey toward heaven."</p>
+            <p>&quotThe Troops of Saint George apostolate aims to use the outdoors as our canvas and the sacraments as our path to light the way for the formation of Holy Catholic men and boys. Whether called to the vocation of the priesthood, the religious life, or that of Holy fatherhood, our fathers and sons will take a prayerful pilgrimage together to fulfill Christ&#39s desire for them to grow in virtue and in their Holy Catholic faith as they journey toward heaven.&quot</p>
           </Row>
         </Container>
       </Layout>
