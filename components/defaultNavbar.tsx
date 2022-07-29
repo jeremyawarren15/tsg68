6d7ba047f28b84aa03479dfcd6698d8f257456ed
Jeremy Warren <jeremyawarren15@gmail.com>
2022-07-29 01:50:19 -0400

    fixed an issue with the navbar shield logo

--- components/defaultNavbar.tsx
+++ components/defaultNavbar.tsx
@@ -12,16 +12,16 @@ const DefaultNavbar: FunctionComponent = () => {
         <Navbar.Brand href="/" className={style.brand}>
           <Image
             alt="TSG Shield Logo"
-            src="TSG_shield.png"
+            src="/TSG_shield.png"
             width="40px"
-            style={{marginRight: "10px"}}
+            height="40px"
           />
-          Troop 68
+          <span className='ms-2'>Troop 68</span>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link href="https://troopsofsaintgeorge.org/wp-content/uploads/2020/09/2020-09-28-TSG-Officers-Manual-2020-Ed.pdf">Officer&#39s Manual</Nav.Link>
+            <Nav.Link href="https://troopsofsaintgeorge.org/wp-content/uploads/2020/09/2020-09-28-TSG-Officers-Manual-2020-Ed.pdf">Officer's Manual</Nav.Link>
           </Nav>
           <Nav>
             <Nav.Link href="https://docs.google.com/forms/d/e/1FAIpQLScvqY4kwtT_bvMHGplOFZ7YYS8KayHzdocagW_HN0TrpraruQ/viewform?usp=sf_link">Register</Nav.Link>
