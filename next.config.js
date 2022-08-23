1a2ff3c35b1138edab4f3866fc3a43ee83967f31
Jeremy Warren <jeremyawarren15@gmail.com>
2022-08-22 21:05:39 -0400

    updated pwa stuff with recommended changes

--- next.config.js
+++ next.config.js
@@ -1,14 +1,15 @@
 const withPWA = require('next-pwa');
+const runtimeCaching = require('next-pwa/cache');
 
 const nextConfig = withPWA({
   reactStrictMode: true,
-  swcMinify: true,
   pwa: {
     dest: 'public',
     register: true,
+    skipWaiting: true,
+    runtimeCaching,
     disable: process.env.NODE_ENV === 'development',
-    scope: '/app',
-    sw: 'service-worker.js',
+    buildExcludes: [/middleware-manifest.json$/]
   }
 });
 
