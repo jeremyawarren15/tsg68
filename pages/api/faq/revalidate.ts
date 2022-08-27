7b81643a7cf706becde2b087e064ed77b54f87ef
Jeremy Warren <jeremyawarren15@gmail.com>
2022-08-26 22:55:37 -0400

    added on-demand revalidation to faqs

---
+++ pages/api/faq/revalidate.ts
@@ -0,0 +1,24 @@
+import { NextApiHandler } from "next";
+
+const handler: NextApiHandler = async (req, res) => {
+  const token = req.query.token;
+  const id = req.body.sys.id;
+
+  if (token !== process.env.REVALIDATION_SECRET) {
+    return res.status(401).json({ message: "You are not authorized to access this endpoint." });
+  }
+
+  if (!id) {
+    return res.status(400).json({ message: "Could not complete request because slug was missing." });
+  }
+
+  try {
+    await res.revalidate('/faq')
+
+    return res.status(200).json({ message: "Revalidation completed." })
+  } catch {
+    return res.status(500).json({ message: "Something went wrong while revalidating." })
+  }
+}
+
+export default handler;
